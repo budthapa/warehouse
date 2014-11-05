092c4e87643cd12a0e2684e880f6d0973bb4ff35
budthapa <budthapa@gmail.com>
2014-11-05 09:38:27 +0545

    form with validation

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -28,18 +28,58 @@ public class Products extends Controller {
 
 	// show product edit form
 	public static Result details(String ean) {
-		return ok(ean);
-
+		//get the product details by its id
+		final Product product=Product.findByEan(ean);
+		if(product==null){
+			return notFound(String.format("Item %s not found", ean));
+		}
+		//fill the empty form with the details found above
+		Form<Product> filledForm=productForm.fill(product);
+		return ok(details.render(filledForm));
+		
 	}
 
 	// save the product
+	/* Flash scope is a place where we can store variables between requests. 
+	 * Everything in flash scope is there until the following request, at which point it’s deleted
+	 * Add flash in main.scala.html template in body part because every page extends it
+	 */
 	public static Result save() {
-		return TODO;
-
+		//getting the form data from details.scala.html
+		Form<Product> getFormData=productForm.bindFromRequest();
+		
+		System.out.println("error here");
+		//check for errors like validation
+		if(getFormData.hasErrors()){
+			//display message
+			flash("error", "Please correct the form below.");
+			return badRequest(details.render(getFormData));
+		}
+		
+		Product product=getFormData.get();
+		//save
+		product.save();
+		//success message
+		flash("success",String.format("Successfully added product %s", product));
+//		return ok(String.format("%s saved successfully", product));
+		return redirect(routes.Products.list());
 	}
 
+	//delete
+	//bit of JavaScript to send a DELETE request in list.scala.html
+	//add the route with verb DELETE in routes file
+	public static Result delete(String ean){
+		final Product product=Product.findByEan(ean);
+		if(product==null){
+			return notFound(String.format("Item %s not found", ean));
+		}
+		
+		Product.remove(product);
+		return redirect(routes.Products.list());
+	}
 	public static Result index() {
 		return TODO;
 	}
 
 }
+																																																
